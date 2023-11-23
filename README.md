0e983bc4052afd9f1f9f6e2945e2e6db6633655a
Huseyin Karatas <huseyin.karatas@metu.edu.tr>
2023-11-24 00:50:35 +0300

    Initial Commit - Created readme.md file

---
+++ README.md
@@ -0,0 +1,103 @@
+# det(ective): Determinant Detective - Matrix Determinant Calculator
+
+## Introduction
+Welcome to the Determinant Detective, a Python program designed to calculate the determinant of a square matrix efficiently. This program ensures user-friendly input validation and error handling to make the process as smooth as possible.
+
+## Table of Contents
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Example](#example)
+- [Algorithm](#algorithm)
+- [Contributing](#contributing)
+
+## Features
+
+- Calculates the determinant of any square matrix.
+- Handles invalid inputs gracefully.
+- Works with matrices of various sizes.
+- Utilizes a recursive algorithm for determinant calculation.
+
+## Getting Started
+
+Before you start using the Determinant Detective, make sure you have the following prerequisites installed on your system:
+
+- Python
+- NumPy library**
+
+You can install NumPy using pip:
+
+```bash
+'pip install numpy'
+```
+
+or Anaconda
+
+```Anacondo Prompt
+    # Best practice, use an environment rather than install in the base env
+    conda create -n my-env
+    conda activate my-env
+    # If you want to install from conda-forge
+    conda config --env --add channels conda-forge
+    # The actual install command
+    conda install numpy
+```
+
+After you installed, activate your own environment and installed numpy via conda, go VSCode and press CTRL + Shift + P. Type:
+```VSCode File Researcher
+> Python: Select Interpreter
+```
+Then choose your own environment.
+Now you can import and use NumPy.
+
+
+## Usage
+
+To use the Determinant Detective, follow these steps:
+
+1. Run the program in your Python environment (e.g., VSCode).
+
+2. You will be greeted with a welcome message and an introduction to the program.
+
+3. Enter the number of rows and columns for your matrix when prompted. Ensure the following conditions are met:
+   - The matrix must have at least one row and one column.
+   - Row and column sizes should be positive integers.
+   - The matrix should be square (i.e., rows equal columns).
+
+4. Enter the values for each element of the matrix as prompted.
+
+5. The program will calculate and display the determinant of the matrix.
+
+## Example
+
+Lets walk through an example of using the Determinant Detective to calculate the determinant of a 3x3 matrix:
+
+```bash
+1 2 3
+4 5 6
+7 8 9
+```
+
+
+1. Run the program and enter `3` for the number of rows and `3` for the number of columns.
+
+2. Enter the values for each element as prompted.
+
+3. The program will display the determinant, which should be `0` for this particular matrix.
+
+## Algorithm
+
+The Determinant Detective uses a recursive algorithm for determinant calculation. Here's how it works:
+
+- For 2x2 matrices, it directly calculates the determinant using the formula:
+
+```bash
+determinant = (a * d) - (b * c)
+```
+
+- For larger matrices, it breaks down the problem into smaller submatrices. It calculates the determinant recursively by calculating the cofactors and submatrices.
+
+## Contributing
+
+If you'd like to contribute to the Determinant Detective, feel free to fork this repository, make improvements, and create a pull request. We welcome enhancements, bug fixes, and additional features.
+
---
+++ det(ective).py
@@ -0,0 +1,88 @@
+import numpy as np
+
+# Welcome Messages
+print('Welcome to the det(ective): Determinant Detective.')
+print('Determinant Detective is able to calculate the determinant of any matrix of any size you will give.')
+
+starter= True
+while (starter):
+    try: 
+        # Take the size of the matrix in terms of rows and columns
+        rows = int(input("Enter the number of rows: "))
+        columns = int(input("Enter the number of columns:"))
+        
+        # Check if the matrix is zero matrix (0x0)
+        if((rows == 0) and (columns == 0)):
+            print('A matrix must have at least one row and one column. Please enter valid dimensions.\n')
+        # Check if any of the size inputs is negative value.
+        elif((rows < 0) or (columns < 0)):
+            print('Please enter postive integer values of row and column size of the matrix.\n')
+        # Check if the given matrix size DOES NOT represent a square matrix.
+        elif (rows != columns):
+            print('Determinant of a matrix A = aij of i!=j is UNDEFINED.')
+            print('Please enter a square matrix size, such as 3x3, 5x5.\n')
+        # If everything is ok:
+        else:
+            break
+    # If any of the inputs is invalid (non-numeric)
+    except ValueError:
+        print("Invalid input. Please enter a valid numerical value.")
+            
+    
+# Initialize an empty matrix filled with zeros
+matrix = np.zeros((rows, columns))
+print('')
+
+# Fill the matrix with user inputs
+for i in range(rows):
+    for j in range(columns):
+        while True:  
+            # Keep prompting until valid input for all the entries is provided
+            try:
+                entry = float(input(f"Enter the value for entry at row {i + 1}, column {j + 1}: "))
+                # Check if the given entry is non-zero
+                if entry >= 0:  
+                    matrix[i, j] = entry
+                    break 
+                else:
+                    print("Please enter a non-negative entry value.")
+            # If any of the inputs is invalid (non-numeric)
+            except ValueError:
+                print("Invalid input. Please enter a valid numerical value.")
+
+# Print the given for information
+print("\nYour Matrix:")
+print(matrix)
+
+
+
+def calculateDeterminant(matrix):
+    # Get the number of rows and columns of the matrix from the user input
+    rows, columns = matrix.shape
+    
+    # Base case: If the matrix is 2x2, return the determinant
+    if rows == 2:
+        return (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0])
+    
+    # Initialize the determinant:
+    determinant = 0
+    
+    # 
+    for j in range(columns):
+        # Calculate the cofactor for the current column
+        cofactor = ((-1) ** j) * (matrix[0, j])
+
+        # Recursive logic for calculating the determinant of the submatrix (remained)
+        submatrix = np.delete(np.delete(matrix, 0, axis=0), j, axis=1)
+        submatrix_determinant = calculateDeterminant(submatrix)
+
+        # Add the contribution of the current column to the determinant
+        determinant += (cofactor * submatrix_determinant)
+
+    return determinant
+
+
+calculated = calculateDeterminant(matrix)
+
+
+print('\nDeterminant of this matrix is: ' + str(calculated))
